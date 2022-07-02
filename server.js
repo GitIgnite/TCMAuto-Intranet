bfc8483f97fa9c2eb770b8b7b735de0a51e8f298
GitIgnite <ravet.florent@gmail.com>
2022-07-02 14:01:13 +0200

    Test déploiement build --prod

--- server.js
+++ server.js
@@ -6,17 +6,12 @@ const compression = require("compression");
 const _app_folder = 'dist/TCMAuto-INTRANET';
 
 const app = express();
-
 const port = process.env.PORT || 8080
 
 // ---- SERVE STATIC FILES ---- //
 console.log("server : "+app)
 console.log("server.server : "+app.server)
 
-app.use('/', express.static('dist/TCMAuto-INTRANET'));
+app.use('/', express.static(_app_folder));
 
-app.listen(port, () => { console.log("app is started and listening port : ", port)})
-// ---- START UP THE NODE SERVER  ----
-// server.listen(_port, function () {
-//   console.log("Node Express server for " + server.name + " listening on http://localhost:" + _port);
-// });
+app.listen(port, () => { console.log("app is started and listening port : ", process.env.HOST)})
