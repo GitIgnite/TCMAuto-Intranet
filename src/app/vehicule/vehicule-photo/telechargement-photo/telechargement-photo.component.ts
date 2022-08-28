4d113d18703f52238c2bb4afba8ea95bca237dd7
Alexiskos <alexisravet@hotmail.fr>
2022-08-28 23:37:45 +0200

    Multiple upload

--- src/app/vehicule/vehicule-photo/telechargement-photo/telechargement-photo.component.ts
+++ src/app/vehicule/vehicule-photo/telechargement-photo/telechargement-photo.component.ts
@@ -1,6 +1,6 @@
 import { HttpEventType, HttpResponse } from '@angular/common/http';
 import {Component, Input, OnInit} from '@angular/core';
-import { Observable } from 'rxjs';
+import { NEVER, Observable } from 'rxjs';
 import { FileImage } from 'src/app/api/models/FileImage';
 import { VehiculePhotoService } from 'src/app/api/services/vehiculePhoto.service';
 import {MatSnackBar} from "@angular/material/snack-bar";
@@ -16,61 +16,70 @@ class ImageSnippet {
   templateUrl: './telechargement-photo.component.html',
   styleUrls: ['./telechargement-photo.component.scss']
 })
+
 export class TelechargementPhotoComponent {
 
-  selectedFiles?: FileList;
+  selectedFiles!: FileList;
   currentFile?: File;
-  fileImage!: FileImage
-  progress = 0;
+  fileImage!: FileImage;
+  progressInfos: any[] = [];
   message = '';
   fileInfos?: Observable<any>;
   messageErrorKeys = MessageErrorKeys
   @Input()
   vehiculeId :string = '';
+  orderPhoto!: number
 
   constructor(private imageService: VehiculePhotoService,
               private readonly _snackBar: MatSnackBar){
   }
 
-  selectFile(event: any): void {
+  ngOnInit(): void {
+  }
+
+  selectFiles(event: any): void {
+    this.progressInfos = [];
     this.selectedFiles = event.target.files;
   }
 
-  upload(): void {
-    console.log("Upload image !!!!")
-    this.progress = 0;
-    console.log("Uplaod file")
+  uploadFiles(): void {
+    for (let i = 0; i < this.selectedFiles.length; i++) {
+      this.upload(i, this.selectedFiles[i]);
+    }
+
+    console.log("Upload file after for");
+  }
+
+  upload(idx: number, file: File): void {
+    console.log("Upload file");
     if (this.selectedFiles) {
-      const file: File | null = this.selectedFiles.item(0);
-      if (file && this.vehiculeId) {
-        this.currentFile = file;
-        this.imageService.upload(this.currentFile, this.vehiculeId).subscribe(
-          (event: any) => {
-            if (event.type === HttpEventType.UploadProgress) {
-              this.progress = Math.round(100 * event.loaded / event.total);
-            } else if (event instanceof HttpResponse) {
-              this.message = event.body.message;
-              //this.fileInfos = this.imageService.getFiles();
-            }
-          },
-          (err: any) => {
-            console.log(err);
-            this.progress = 0;
-            if (err.error && err.error.message) {
-              this.message = err.error.message;
-            } else {
-              this.message = 'Could not upload the file!';
-            }
-            this.currentFile = undefined;
-          });
-      } else {
-        if(!this.vehiculeId) {
-          this._snackBar.open(this.messageErrorKeys.ERREUR_UPLOAD_IMAGE_ID_VEHICULE_NULL, 'Erreur');
+      this.progressInfos[idx] = { value: 0, fileName: file.name };
+        if (file && this.vehiculeId) {
+          this.imageService.upload(file, this.vehiculeId).subscribe(
+            (event: any) => {
+              if (event.message) {
+                this.progressInfos[idx] = {value: Math.round(100 * event.loaded / event.total)};
+              } else if (event instanceof HttpResponse) {
+                this.message = event.body.message;
+                //this.imageService.getPhotoByIdVehicule(this.vehiculeId)
+              }
+            },
+            (err: any) => {
+              console.log(err);
+              this.progressInfos[idx] = {value: 0};
+              if (err.error && err.error.message) {
+                this.message = err.error.message;
+              } else {
+                this.message = 'Impossible d\'upload ce fichier: '+ file.name + ' !';
+              }
+            });
         } else {
-          this._snackBar.open(this.messageErrorKeys.ERREUR_UPLOAD_AUCUNE_IMAGE_SELECTIONNEE, 'Erreur');
+          if(!this.vehiculeId) {
+            this._snackBar.open(this.messageErrorKeys.ERREUR_UPLOAD_IMAGE_ID_VEHICULE_NULL, 'Erreur');
+          } else {
+            this._snackBar.open(this.messageErrorKeys.ERREUR_UPLOAD_AUCUNE_IMAGE_SELECTIONNEE, 'Erreur');
+          }
         }
-      }
-      this.selectedFiles = undefined;
     }
   }
 }
