dd3f9239ebc736be0c2172e59e23ac91cf4dd695
GitIgnite <ravet.florent@gmail.com>
2022-11-09 22:00:39 +0100

    update password

---
+++ src/app/common/component/update-password/update-password.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-password',
+  templateUrl: './update-password.component.html',
+  styleUrls: ['./update-password.component.scss']
+})
+export class UpdatePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  updatePassword(){
+
+  }
+
+  cancelAndCloseComponent() {
+
+  }
+}
